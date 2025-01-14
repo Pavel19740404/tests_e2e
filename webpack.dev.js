df32e501167df78145f5001ff139b5e990ebc599
Pavel19740404 <74202355+Pavel19740404@users.noreply.github.com>
2025-01-14 15:59:05 +0300

    Update webpack.dev.js

--- webpack.dev.js
+++ webpack.dev.js
@@ -10,6 +10,7 @@ module.exports = merge(common, {
 
   // Spin up a server for quick development
   devServer: {
+    port: 8080,
     historyApiFallback: true,
     open: true,
     compress: true,
